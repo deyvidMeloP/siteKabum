679db6bf760f04910501965037d4bfc77e272ca7
deyvidMeloP <158530281+deyvidMeloP@users.noreply.github.com>
2024-07-14 19:01:20 -0300

    Add files via upload

---
+++ browser/assets/siblings_Swiper.js
@@ -0,0 +1,15 @@
+
+function siblings_Swiper() {
+
+    const swiper = new Swiper('.swiper_All', {
+     
+      slidesPerView: 5,
+   
+      navigation: {
+        nextEl: '.siblings_Back',
+        prevEl: '.siblings_Next',
+      },
+    
+  
+    });
+      }
